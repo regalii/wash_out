f92eaf89c6e2569483a07559325e90974d80319d
Jorge Daniel Sampayo Vargas <jdsampayo@gmail.com>
2017-04-03 18:23:40 -0400

    Revert "Fix keys for HSBC"
    This reverts commit 004c58b2a530f4a1411473ac615d3589f2ad52e9.

--- lib/wash_out/dispatcher.rb
+++ lib/wash_out/dispatcher.rb
@@ -55,16 +55,15 @@ module WashOut
         end
       end
 
-      hash.with_indifferent_access
+      hash
     end
 
     # Creates the final parameter hash based on the request spec and xml_data from the request
     def _load_params(spec, xml_data)
       params = HashWithIndifferentAccess.new
       spec.each do |param|
-        key = param.raw_name.to_s
-
-        if xml_data.has_key?(key)
+        key = param.raw_name.to_sym
+        if xml_data.has_key? key
           params[param.raw_name] = param.load(xml_data, key)
         end
       end
@@ -256,10 +255,7 @@ module WashOut
     def xml_data
       envelope = request.env['wash_out.soap_data'].values_at(:envelope, :Envelope).compact.first
       xml_data = envelope.values_at(:body, :Body).compact.first || {}
-      if soap_config.wsdl_style == "document"
-        xml_data = xml_data.map { |k, str| [k.to_s.remove("soap_"), str] }.to_h
-        return xml_data
-      end
+      return xml_data if soap_config.wsdl_style == "document"
       xml_data = xml_data.values_at(soap_action.underscore.to_sym, soap_action.to_sym, request_input_tag.to_sym).compact.first || {}
     end
 
